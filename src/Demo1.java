6bc6bd5009a51287307487680f59e46af65c98f6
rmara <rmara@RobertoPC>
2025-01-29 19:58:34 -0600

    implemented add()

--- src/Demo1.java
+++ src/Demo1.java
@@ -2,11 +2,21 @@
 public class Demo1 {
 
 	public static void main(String[] args) {
-		
-		System.out.println("***");
-		
-		
+
+		System.out.println("RobertoMaradiaga");
+
+		int a = 10;
+		int b = 20;
+
+		int result = a + b;
+		System.out.println(result);
+
 	}
-	
-	
+
+	public void add(int a, int b) {
+
+		System.out.println(a + b);
+
+	}
+
 }
